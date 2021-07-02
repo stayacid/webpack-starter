426108b19807720ddb7d4e18adb18e197a4e87d2
stayacid <stayacid@gmail.com>
2021-07-02 16:41:55 +0300

    start

---
+++ src/ts/index.ts
@@ -0,0 +1,3 @@
+import {test} from './modules/ui';
+
+test();
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, './src/ts/index.ts'),
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      // Inject the js bundle at the end of the body of the given template
+      inject: 'body',
+  }),
+  ],
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
